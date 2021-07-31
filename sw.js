9cf169ccdc86714c7a602db77b5f42cad5a2f6e4
Omzlaw <46542420+Omzlaw@users.noreply.github.com>
2021-07-31 21:05:30 +0100

    add service worker

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./", "styles.css", "./images/logo.png"
+            ])
+        })
+    )
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    )
+});
